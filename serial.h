ee8f4364cbe41ca155a3d9a07d6b95d09e3652d3
Bastian Gschrey <b.gschrey@gmail.com>
2016-01-21 17:33:43 +0100

    first serial functions

--- serial.h
+++ serial.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2016 Markus Ippy, Bastian Gschrey
+ *
+ * Digital Gauges for Apexi Power FC for RX7 on Raspberry Pi
+ *
+ *
+ * This software comes under the GPL (GNU Public License)
+ * You may freely copy,distribute etc. this as long as the source code
+ * is made available for FREE.
+ *
+ * No warranty is made or implied. You use this program at your own risk.
+ */
+
+/*!
+  \file raspexi/powerfc.h
+  \brief Raspexi Viewer Power FC related functions
+  \author Suriyan Laohaprapanon & Jacob Donley
+ */
+
+
+
 #ifndef SERIAL_H
 #define SERIAL_H
 
@@ -13,8 +34,42 @@ class serial : public QObject
 public:
 
     /*
+     * Source: http://www.rx7.org/public/PowerFC-DataLogIt.htm
+     * ------------------
+     * "Advanced" Information (Cooked Averages?)
+     * ------------------
+     * 0xF0
+     * ------------------
+     * short word : rpm (just as a short word, not worked over)
+     * short word : pim (don't know... FCEdit just displays it as a short word)
+     * short word : pim voltage * 1000.0f (divide by 1000.0f to get the voltage)
+     * short word : tps voltage * 1000.0f (divide by 1000.0f to get the voltage)
+     * short word : InjFP ms (short float)
+     * short word : INJ (inj what? dunno... FCEdit shows it as a short word...)
+     * byte : IGL (byte degree)
+     * byte : IGT (byte degree)
+     * byte : Fuel Temperature (as byte temperature)
+     * byte : oil (FCEdit displays as just a byte...)
+     * byte : Precontrol % (FCEdit calls it a %, but looks like it's just displaying the byte)
+     * byte : Wastegate % (FCEdit calls it a %, but looks like it's just displaying the byte)
+     * byte : water temperature (as byte temperature)
+     * byte : air temperature (as byte temperature)
+     * byte : knock
+     * byte : battery voltage * 10.0f (divide by 10.0f to get voltage)
+     *
+     * NOTE: From here, FCEdit is unsure of what's what... Consequently, I'm unsure of what's what.
+     *
+     * short word : kph (don't know the units yet)
+     * short word : unknown
+     * byte : O2
+     * byte : unknown
+     * short word : InjFS (ms)
+     */
+
+    /*
     *Structure for FD3S information :
     */
+
     typedef struct {
         unsigned short RPM;				//0
         unsigned short Intakepress;		//1
