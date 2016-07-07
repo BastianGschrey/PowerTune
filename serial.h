20d4bcedd490e0678863e34894464d34e9fc1e55
MarkusIppy <Markus.Ippy1981@gmail.com>
2016-07-07 15:44:58 +0200

    Implemented Rev/Idle Display and various other map read requests

--- serial.h
+++ serial.h
@@ -44,6 +44,46 @@ public:
     void getSensorData();
     void getMapIndices();
     void getBasic();
+    void getRevIdle();
+    void getSensorStrings();
+    void getPimStrInjA();
+    void getVersion();
+    void getMapRef();
+    void getLeadign1();
+    void getLeadign2();
+    void getLeadign3();
+    void getLeadign4();
+    void getTrailIgn1();
+    void getTrailIgn2();
+    void getTrailIgn3();
+    void getTrailIgn4();
+    void getInitPlatform();
+    void getInjOverlap();
+    void getInjvsFuelT();
+    void getTurboTrans();
+    void getOilervsWaterT();
+    void getFanvsWater();
+    void getInjcorr1();
+    void getInjcorr2();
+    void getInjcorr3();
+    void getInjcorr4();
+    void getFuelInj();
+    void getCranking();
+    void getWaterTcorr();
+    void getInjvsWaterBoost();
+    void getInjvsAirTBoost();
+    void getInjPrimaryLag();
+    void getAccInj();
+    void getInjvsAccel();
+    void getIgnvsAircold();
+    void getIgnvsWater();
+    void getIgnvsAirwarm();
+    void getLIgnvsRPM();
+    void getIgnvsBatt();
+    void getBoostvsIgn();
+
+
+
 
 
 private:
