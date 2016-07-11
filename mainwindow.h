042bb0d2951ab8912824f09277e0c0e9b6317d11
MarkusIppy <Markus.Ippy1981@gmail.com>
2016-07-11 15:03:35 +0200

    More Data Visualition and decoding
    Evaluate and display the following Messages in Tab Settings5 
    Injector Settings 
    Injector Primary Lag (ms) vs Battery Voltage
    Injector Secondary Lag (ms) vs Battery Voltage
    Injector Overlap

--- mainwindow.h
+++ mainwindow.h
@@ -165,11 +165,103 @@ public:
         fc_TurboTrans_info_t parse(const QByteArray &);
     };
 
+     double packageBoostControl[12];
+
+     struct fc_BoostCont_info_t{
+
+        quint16 requesttype;
+        quint8 Setting1;
+        quint8 Setting2;
+        quint8 Setting3;
+        quint8 BoostPrimary1;
+        quint8 BoostSecondary1;
+        quint8 BoostPrimary2;
+        quint8 BoostSecondary2;
+        quint8 DutyPrimary1;
+        quint8 DutySecondary1;
+        quint8 DutyPrimary2;
+        quint8 DutySecondary2;
+        quint8 checksum;
+
+        fc_BoostCont_info_t parse(const QByteArray &);
+    };
+     double packageInjOverlap[12];
+
+     struct fc_InjOverlap_info_t{
+
+        quint16 requesttype;
+        quint8 InjOvBoost1;
+        quint8 lineInjOvSet1;
+        quint8 InjOvBoost2;
+        quint8 lineInjOvSet2;
+        quint8 InjOvBoost3;
+        quint8 lineInjOvSet3;
+        quint8 checksum;
+        fc_InjOverlap_info_t parse(const QByteArray &);
+    };
+     double packageInjPriLagvsBattV[12];
+
+     struct fc_InjPriLagvsBattV_info_t{
+
+        quint16 requesttype;
+        quint16 InjPriLag16V;
+        quint16 InjPriLag14V;
+        quint16 InjPriLag12V;
+        quint16 InjPriLag10V;
+        quint16 InjPriLag8V;
+        quint16 InjPriLag6V;
+        quint8 checksum;
+        fc_InjPriLagvsBattV_info_t parse(const QByteArray &);
+    };
+
+     double packageInjScLagvsBattV[12];
+
+     struct fc_InjScLagvsBattV_info_t{
+
+        quint16 requesttype;
+        quint16 InjScLag16V;
+        quint16 InjScLag14V;
+        quint16 InjScLag12V;
+        quint16 InjScLag10V;
+        quint16 InjScLag8V;
+        quint16 InjScLag6V;
+        quint8 checksum;
+        fc_InjScLagvsBattV_info_t parse(const QByteArray &);
+    };
+     double packageFuelInjectors[26];
+
+     struct fc_FuelInjectors_info_t{
+
+        quint16 requesttype;
+        quint8 empty1;
+        quint8 frontpulse; //(for percent multiply by 100 to get percentage)
+        quint8 empty2;
+        quint8 rearpulse; //(for percent multiply by 100 to get percentage)
+        quint8 frntprilag; //(multiply by 0.004)
+        quint8 empty3;
+        quint8 frntseclag; // (multiply by 0.004)
+        quint8 empty4;
+        quint8 rearprilag; //  (multiply by 0.004)
+        quint8 empty5;
+        quint8 rearseclag; // (multiply by 0.004)
+        quint8 empty6;
+        quint16 prinjsize;
+        quint16 secinjsize;
+        quint16 prisectransprc; // (divide it by 10to get the percentage)
+        quint16 prisectransms;
+        quint8 empty7;
+        quint8 empty8;
+        quint8 empty9;
+        quint8 empty10;
+        quint8 checksum;
+        fc_FuelInjectors_info_t parse(const QByteArray &);
+    };
+
 
 
     // FD3S
-      #define FC_INFO_MUL		{1, 0.0001, 1, 1, 1.0/256, 1.0/256, 1, 1, 1, 212.0/256, 0.4, 0.4, 1, 1, 1, 0.1, 1, 0.1, 0.02, 1, 1.0/256, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 5.0/255, 5.0/255, 5.0/255, 5.0/255, 0.01, 0.001, 0.019, 40}
-      #define FC_INFO_ADD		{0,-1.0332, 0, 0, 0, 0, -25, -25, -80, 0, 0, 0, -80, -80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+      #define FC_INFO_MUL		{1, 0.0001, 1, 1, 1.0/256, 1.0/256, 1, 1, 1, 212.0/256, 0.4, 0.4, 1, 1, 1, 0.1, 1, 0.1, 0.02, 1, 1.0/256, 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 5.0/255, 5.0/255, 5.0/255, 5.0/255, 0.01, 0.001, 0.019, 40, 0.05, 0.004 , 256, -0.00390625, 1.0/128,100}
+      #define FC_INFO_ADD		{0,-1.0332, 0, 0, 0, 0, -25, -25, -80, 0, 0, 0, -80, -80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -128}
 
 private slots:
     void on_btnSerialSettings_clicked();
@@ -198,6 +290,11 @@ private slots:
     void decodeInjcorr4(QByteArray serialdata);
     void decodeVersion(QByteArray serialdata);
     void decodeInit(QByteArray serialdata);
+    void decodeBoostCont(QByteArray serialdata);
+    void decodeInjOverlap(QByteArray serialdata);
+    void decodeInjPriLagvsBattV(QByteArray serialdata);
+    void decodeInjScLagvsBattV(QByteArray serialdata);
+    void decodeFuelInjectors(QByteArray serialdata);
 
 private:
     Ui::MainWindow *ui;
