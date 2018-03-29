f104ca22ddb47a9a0470910c328c164a4ce7f406
BastianGschrey <b.gschrey@gmail.com>
2018-03-29 13:47:09 +0200

    debugging

--- Gauges/createsquaregauge.js
+++ Gauges/createsquaregauge.js
@@ -20,13 +20,13 @@ function finishCreation(setWidth,setHeight,setX,setY,setMaxValue,setDecPlace,set
         console.log("creating object");
         gauge = component.createObject(fueltechDash, {"id": setID, "title": setID, "width": setWidth, "height": setHeight,
                                            //"maintextvalue": Qt.binding(function(){return SetValueObject[SetValuePropertyMain].toFixed(setDecPlace)}),
-                                           //"secvalue": Qt.binding(function(){return SetValueObject[SetValuePropertySec].toFixed(setDecPlace)}),
                                            //"mainvalue": Qt.binding(function(){return SetValueObject[SetValuePropertyMain]}),
-                                           //"maxvalue": setMaxValue,
-                                           //"mainunit": setUnit,
-                                           //"vertgaugevisible": setVertGaugeVis,
-                                           //"horigaugevisible": setHoriGaugeVis,
-                                           //"secvaluevisible": setSecValueVis,
+                                           //"secvalue": Qt.binding(function(){return SetValueObject[SetValuePropertySec].toFixed(setDecPlace)}),
+                                           "maxvalue": setMaxValue,
+                                           "mainunit": setUnit,
+                                           "vertgaugevisible": setVertGaugeVis,
+                                           "horigaugevisible": setHoriGaugeVis,
+                                           "secvaluevisible": setSecValueVis,
                                            "x": setX, "y": setY});
         console.log("creating object done");
         if (gauge == null) {
--- Gauges/fueltechdashboard.qml
+++ Gauges/fueltechdashboard.qml
@@ -14,7 +14,7 @@ id: fueltechDash
 //prototype of function:
 //createVerticalGauge(setWidth,setHeight,setX,setY,setMaxValue,setDecPlace,setUnit,setID,setVertGaugeVis,setHoriGaugeVis,setSecValueVis,SetValueObject,SetValuePropertyMain,SetValuePropertySec)
 Component.onCompleted:
-    CreateSquareGaugeScript.createSquareGauge(200,150,10,180,300,0,"kPa","MAP",1,0,0,"Dashboard","MAP","MAP"); /*
+    CreateSquareGaugeScript.createSquareGauge(200,150,10,180,300,0,"kPa","MAP",true,false,false,"Dashboard","MAP","MAP"); /*
     CreateSquareGaugeScript.createSquareGauge(165,150,220,180,300,2,"λ","Lambda",false,false,true,"Dashboard","LAMBDA","LAMBDATarget"),
     CreateSquareGaugeScript.createSquareGauge(200,120,10,340,150,0,"°C","MAT",false,true,false,"Dashboard","Intaketemp","Intaketemp"),
     CreateSquareGaugeScript.createSquareGauge(340,120,220,340,300,0,"°C","ECT",false,true,false,"Dashboard","Watertemp","Watertemp"),
