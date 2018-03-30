347eb7d7be9aac94524f468dc7f6e38b9ad43c07
Unknown <Markus.Ippy1981@gmail.com>
2018-03-30 21:12:04 +0200

    removed Debug messages

--- connect.cpp
+++ connect.cpp
@@ -131,7 +131,6 @@ void Connect::checkifraspberrypi()
 
 void Connect::readdashsetup()
 {
-    qDebug()<<"c++ file read";
     QString path = "UserDash.txt";// this is just for testing
     QFile inputFile(path);
     if (inputFile.open(QIODevice::ReadOnly))
@@ -142,7 +141,6 @@ void Connect::readdashsetup()
           QString line = in.readLine();
           QStringList list = line.split(QRegExp("\\,"));
           m_dashBoard->setdashsetup(list);
-          qDebug()<< list;
        }
        inputFile.close();
     }
