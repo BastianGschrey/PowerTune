4eb64930a9eee5cc3bbcfd0412e3eae700771313
Markus Ippy <Markus.Ippy1981@gmail.com>
2019-04-21 09:00:00 +0200

    fix

--- Gauges/createverticalbargauge.js
+++ Gauges/createverticalbargauge.js
@@ -3,7 +3,7 @@ var component;
 var gauge;
 
 function createVerticalGauge(setWidth,setHeight,setX,setY,setMinValue,setMaxValue,setDecPlace,setUnit,SetValueProperty,SetWarnvaluehigh,Setwarnvaluelow) {
-    component = Qt.createComponent("verticalbargauge.qml");
+    component = Qt.createComponent("Verticalbargauge.qml");
     if (component.status === Component.Ready)
         finishCreation(setWidth,setHeight,setX,setY,setMinValue,setMaxValue,setDecPlace,setUnit,SetValueProperty,SetWarnvaluehigh,Setwarnvaluelow);
     else
