12e67d5e56047bfddf358f3c7b50f49dccf4d6aa
MarkusIppy <Markus.Ippy1981@gmail.com>
2019-09-18 20:46:23 +0000

    udate script

--- updatedaemons.sh
+++ updatedaemons.sh
@@ -12,6 +12,7 @@ sudo pkill Microtechd
 sudo pkill Linkd
 sudo pkill Hondatad
 sudo pkill AdaptronicCANd
+sudo pkill AEMV2d
 echo "Updating Daemons"
 cp -r /home/pi/src/daemons/. /home/pi/daemons/
 cd /home/pi/daemons
