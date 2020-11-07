d0f38a7b6379e3dff1f12c9cd9e22e485872dc05
MarkusIppy <Markus.Ippy1981@gmail.com>
2020-11-07 01:03:05 +0000

    Fix script

--- updateUserDashboards.sh
+++ updateUserDashboards.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-echo "Checking if Bull Dash1 is installed"
-rsync -a -v --ignore-existing /home/pi/src/exampleDash/Logo/Bull_Style1.png /home/pi/Logo/
-rsync -a -v --ignore-existing /home/pi/src/exampleDash/UserDashboards/Bull_Style1.txt /home/pi/Userdashboards/
-echo "Checking if Bull Dash2 is installed"
-rsync -a -v --ignore-existing /home/pi/src/exampleDash/Logo/Bull_Style2.png /home/pi/Logo/
-rsync -a -v --ignore-existing /home/pi/src/exampleDash/UserDashboards/Bull_Style2.txt /home/pi/Userdashboards/
-echo "Checking if MFD Dash is installed "
-rsync -a -v --ignore-existing /home/pi/src/exampleDash/Logo/MFD.png /home/pi/Logo/
-rsync -a -v --ignore-existing /home/pi/src/exampleDash/UserDashboards/MFD.txt /home/pi/Userdashboards/
+echo "copy new Dashboards"
+rsync -a -v --ignore-existing /home/pi/src/exampleDash/Logo/*.png /home/pi/Logo/
+rsync -a -v --ignore-existing /home/pi/src/exampleDash/UserDashboards/*.txt /home/pi/UserDashboards/
+
