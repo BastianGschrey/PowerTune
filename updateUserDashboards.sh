e750af8a03204175be72b2b87c684e872c5698a9
MarkusIppy <Markus.Ippy1981@gmail.com>
2020-11-08 11:10:30 +0000

    Add Fix for MFD Txt

--- updateUserDashboards.sh
+++ updateUserDashboards.sh
@@ -3,3 +3,7 @@ echo "copy new Dashboards"
 rsync -a -v --ignore-existing /home/pi/src/exampleDash/Logo/*.png /home/pi/Logo/
 rsync -a -v --ignore-existing /home/pi/src/exampleDash/UserDashboards/*.txt /home/pi/UserDashboards/
 
+echo "fix MFD"
+cp /home/pi/src/exampleDash/UserDashboards/MFD.txt /home/pi/UserDashboards/
+
+
