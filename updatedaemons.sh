2ad5ac5e6f3b8354cf3b1bf6b1c358b02638903b
MarkusIppy <Markus.Ippy1981@gmail.com>
2021-02-15 08:35:45 +0000

    ECOBOOST daemon

--- updatedaemons.sh
+++ updatedaemons.sh
@@ -38,6 +38,7 @@ sudo pkill MegasquirtCan
 sudo pkill EMSCAN
 sudo pkill WRX2012
 sudo pkill Testdaemon
+sudo pkill ecoboost
 
 echo "Removing previous Versions"
 
@@ -74,6 +75,7 @@ sudo rm /home/pi/daemons/GMCANd
 sudo rm /home/pi/daemons/MegasquirtCan
 sudo rm /home/pi/daemons/WRX2012
 sudo rm /home/pi/daemons/Testdaemon
+sudo rm /home/pi/daemons/ecoboost
 
 echo "Updating Daemons"
 cp -r /home/pi/src/daemons/. /home/pi/daemons/
