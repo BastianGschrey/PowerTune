4f7a661d1183a9dea4abe800265093e0a7c9b650
PowerTune Digital <info@powertunedigital.com>
2022-03-31 10:14:36 +0200

    test

--- wifiscanner.cpp
+++ wifiscanner.cpp
@@ -29,21 +29,7 @@ WifiScanner::WifiScanner(DashBoard *dashboard, QObject *parent)
 void WifiScanner::initializeWifiscanner()
 {
 
-    qDebug() << "Query WLAN IP Adress" ;
-    QNetworkInterface wlan0IP = QNetworkInterface::interfaceFromName("wlan0");
-    QList<QNetworkAddressEntry> entries = wlan0IP.addressEntries();
-    if (!entries.isEmpty()) {
-        QNetworkAddressEntry entry = entries.first();
-        qDebug() << "wlan0 IP" <<entry.ip();
-    }
 
-    qDebug() << "Query ETH IP Adress" ;
-    QNetworkInterface eth0IP = QNetworkInterface::interfaceFromName("eth0");
-    QList<QNetworkAddressEntry> test = eth0IP.addressEntries();
-    if (!test.isEmpty()) {
-        QNetworkAddressEntry entry2 = test.first();
-        qDebug() << "etH0 IP" <<entry2.ip();
-    }
     process = new QProcess(this);  // create on the heap, so it doesn't go out of scope
     connect (process, SIGNAL(readyReadStandardOutput()), this, SLOT(readData()));  // connect process signals with your code
     connect(process, SIGNAL(finished(int , QProcess::ExitStatus )), this, SLOT(finalize(int , QProcess::ExitStatus)));
@@ -70,6 +56,7 @@ void WifiScanner::checkWifiIP()
     proc.waitForFinished();
     QString output = proc.readAllStandardOutput();
     //m_dashBoard->setSerialStat("Rebooting");
+
 
 }
 void WifiScanner::findActiveWirelesses()
@@ -114,6 +101,22 @@ void WifiScanner::findActiveWirelesses()
     proc3.waitForFinished();
     QString output3 = proc3.readAllStandardOutput();
     m_dashboard->setSerialStat(output3);
+
+    qDebug() << "Query WLAN IP Adress" ;
+    QNetworkInterface wlan0IP = QNetworkInterface::interfaceFromName("wlan0");
+    QList<QNetworkAddressEntry> entries = wlan0IP.addressEntries();
+    if (!entries.isEmpty()) {
+        QNetworkAddressEntry entry = entries.first();
+        qDebug() << "wlan0 IP" <<entry.ip();
+    }
+
+    qDebug() << "Query ETH IP Adress" ;
+    QNetworkInterface eth0IP = QNetworkInterface::interfaceFromName("eth0");
+    QList<QNetworkAddressEntry> test = eth0IP.addressEntries();
+    if (!test.isEmpty()) {
+        QNetworkAddressEntry entry2 = test.first();
+        qDebug() << "etH0 IP" <<entry2.ip();
+    }
 
 
 }
