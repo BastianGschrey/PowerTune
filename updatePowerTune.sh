313a83dd39d74fcdc8ff0fd00705d4fd48653394
MarkusIppy <Markus.Ippy1981@gmail.com>
2022-04-06 12:25:35 +0000

    update script modified

--- updatePowerTune.sh
+++ updatePowerTune.sh
@@ -1,4 +1,55 @@
 #!/bin/sh
+#Check if this is a Yocto image 
+if [ -d /home/root ]; then
+# Get the latest source
+		echo "Yocto detected "
+		if [ -d /home/pi/src ]; then
+		echo "Updating to latest source "
+		cd /home/pi/src
+		git reset --hard
+		git clean -fd
+		git pull
+		./updatedaemons.sh
+		./updateUserDashboards.sh
+		else
+		echo "Create source directory and clone PowerTune Repo"
+		mkdir /home/pi/src
+		git clone https://github.com/BastianGschrey/PowerTune.git /home/pi/src  
+		cd src
+		./updatedaemons.sh
+		./updateUserDashboards.sh
+		fi
+# Check if the Logo Folder Exists
+		if [ -d /home/pi/Logo ]; then
+		echo "Logo folder exists"
+		else
+		echo "Create Logo Folder"
+		mkdir /home/pi/Logo
+		fi
+# Check if there is a build folder
+		if [ -d /home/pi/build ]; then
+		echo "Delete previous build folder"
+		sudo rm -r /home/pi/build
+		mkdir /home/pi/build
+		else
+		mkdir /home/pi/build
+		fi
+# Compile PowerTune
+		cd /home/pi/build
+		echo "Compiling PowerTune ... go grab a Coffee"
+		qmake /home/pi/src
+		make -j4
+# Check if the PowerTune executable exists in the build folder
+		if [ -f /home/pi/build/PowertuneQMLGui ];then
+		echo "Successfully compiled"
+		sudo reboot
+		else
+		echo "Something went wrong"
+		sudo rm -r /home/pi/build
+		fi
+
+# Raspbian image 
+else
 if nc -zw5 www.github.com 443; then
 # Get the latest source
 		if [ -d /home/pi/src ]; then
@@ -53,4 +104,4 @@ if nc -zw5 www.github.com 443; then
 else
 echo "Update not possible , Github not reachable check your connection "
 fi
-
+fi
