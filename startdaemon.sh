98a396a779bf1b66d8cdfc37b9c67f6dc47d19ef
Markus Ippy <Markus.Ippy1981@gmail.com>
2022-04-17 07:45:27 +0200

    Delete startdaemon.sh

--- startdaemon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo ifdown can0
-sudo ifup can0
-#PLMS Consult Cable drivers
-sudo modprobe ftdi_sio
-sudo sh -c 'echo "0403 c7d9" > /sys/bus/usb-serial/drivers/ftdi_sio/new_id'
-cd /home/pi/daemons
-./Link
-
